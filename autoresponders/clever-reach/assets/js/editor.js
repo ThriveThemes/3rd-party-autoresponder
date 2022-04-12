ab684a5450844d8a375b4ec67f8844a9c9ac2f41
Istvan <kovacsistvan1202@gmail.com>
2022-04-12 13:46:02 +0300

    Small improvements

--- autoresponders/clever-reach/assets/js/editor.js
+++ autoresponders/clever-reach/assets/js/editor.js
@@ -2,7 +2,9 @@
  * This contains functionality that allows the tags and form settings to show in the Thrive Architecr editor in the Lead Generation API settings.
  */
 if ( typeof TVE !== 'undefined' ) {
-	const $mainEditorContainer = TVE.$( TVE.main );
+	/* if you re-use this for another API, everything should still work :) */
+	const CLEVER_REACH_API_KEY = thrive_third_party_api_localized_data.api_key,
+		$mainEditorContainer = TVE.$( TVE.main );
 
 	/**
 	 * In order to add the API Tag functionality, we must add the tag controls to the API controls template.
@@ -12,7 +14,7 @@ if ( typeof TVE !== 'undefined' ) {
 	 * @param {Object} model
 	 */
 	TVE.add_action( 'tcb.lead_generation.api_settings_template', ( $template, apiKey, model ) => {
-		if ( apiKey === 'clever-reach' ) {
+		if ( apiKey === CLEVER_REACH_API_KEY ) {
 			/* Functionality for forms */
 			$template.append( TVE.tpl( 'lead-generation/apis/default-form-controls' )( {api: model} ) );
 			/* Functionality for tags */
@@ -26,7 +28,7 @@ if ( typeof TVE !== 'undefined' ) {
 	 * @param {Event} event
 	 * @param {Object} params
 	 */
-	$mainEditorContainer.on( 'tve-api-after-render-clever-reach.tcb', ( event, params ) => {
+	$mainEditorContainer.on( `tve-api-after-render-${CLEVER_REACH_API_KEY}.tcb`, ( event, params ) => {
 		const $container = params.$container,
 			$listSelect = $container.find( '.api-list' ),
 			$formSelect = $container.find( '.tcb-form-list' ),
@@ -45,6 +47,7 @@ if ( typeof TVE !== 'undefined' ) {
 				if ( forms && forms[ selectedValue ] ) {
 					const selectedForm = api.getConfig( 'form' ) || '';
 
+					/* Add the forms to the forms select  */
 					_.each( forms[ selectedValue ], form => {
 						$formSelect.append( new Option( form.name, form.id, true, selectedForm === form.id ) );
 					} );
@@ -52,7 +55,9 @@ if ( typeof TVE !== 'undefined' ) {
 					hasForms = true;
 				}
 
+				/* the forms select is only displayed if we have something to populate it with */
 				$formSelect.toggle( hasForms );
+				/* if no forms were found, display the 'No forms available' error, otherwise hide the error text */
 				$container.find( '.tcb-forms-error' ).toggle( ! hasForms );
 			};
 
@@ -65,7 +70,7 @@ if ( typeof TVE !== 'undefined' ) {
 	 * Functionality for tags and forms.
 	 * When the API settings are saved, also save the selected tags and the selected form.
 	 */
-	$mainEditorContainer.on( 'tve-api-options-clever-reach.tcb', ( event, params ) => {
+	$mainEditorContainer.on( `tve-api-options-${CLEVER_REACH_API_KEY}.tcb`, ( event, params ) => {
 		/* 'get_inputs_value' is a Thrive Architect function that reads the selected values */
 		params.api.setConfig( TVE.get_inputs_value( params.$container, '.tve-api-extra' ) );
 	} );
@@ -77,7 +82,7 @@ if ( typeof TVE !== 'undefined' ) {
 	 * @returns {String} logo
 	 */
 	TVE.add_filter( 'tcb.lead_generation.api_logo', ( logo, apiKey ) => {
-		if ( apiKey === 'clever-reach' ) {
+		if ( apiKey === CLEVER_REACH_API_KEY ) {
 			logo = thrive_third_party_api_localized_data.api_logo;
 		}
 
